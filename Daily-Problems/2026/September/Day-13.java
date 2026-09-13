d6da9202606e3497873bbd67f1c04cbcccd340ab
Tithi Agarwal <209246782+Tithi-Agarwal@users.noreply.github.com>
2026-09-14 00:30:04 +0530

    DP-13-835

--- Daily-Problems/2026/September/Day-13.java
+++ Daily-Problems/2026/September/Day-13.java
@@ -1,35 +1,93 @@
 //835. Image Overlap
 import java.util.*;
 
+class Pair {
+    int row, col;
+
+    Pair(int r, int c) {
+        row = r;
+        col = c;
+    }
+}
+
 class Solution {
-    public int largestOverlap(int[][] img1, int[][] img2) {
+
+    public int check(ArrayList<Pair> path, int[][] mat, int r, int c)
+    {
+        int count = 0;
+
+        if(r >= 0 && r < mat.length &&
+           c >= 0 && c < mat[0].length &&
+           mat[r][c] == 1)
+        {
+            count = 1;
+        }
+
+        for(int i = 0; i < path.size(); i++)
+        {
+            Pair ele = path.get(i);
+
+            r = r + ele.row;
+            c = c + ele.col;
+
+            if(r >= mat.length || c >= mat[0].length ||
+               r < 0 || c < 0)
+                continue;
+
+            if(mat[r][c] == 1)
+                count++;
+        }
+
+        return count;
+    }
+
+    public int largestOverlap(int[][] img1, int[][] img2)
+    {
         int n = img1.length;
+        int m = img1[0].length;
 
-        List<int[]> ones1 = new ArrayList<>();
-        List<int[]> ones2 = new ArrayList<>();
+        int c = 0;
+        int prevr = 0;
+        int prevc = 0;
 
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                if (img1[r][c] == 1) ones1.add(new int[]{r, c});
-                if (img2[r][c] == 1) ones2.add(new int[]{r, c});
+        ArrayList<Pair> path = new ArrayList<>();
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(img2[i][j] == 1 && c == 0)
+                {
+                    prevr = i;
+                    prevc = j;
+                    c++;
+                    continue;
+                }
+
+                if(img2[i][j] == 1 && c > 0)
+                {
+                    path.add(new Pair(i - prevr, j - prevc));
+
+                    prevr = i;
+                    prevc = j;
+                }
             }
         }
 
-        Map<Integer, Integer> frequency = new HashMap<>();
-        int maxOverlap = 0;
+        if(c == 0)
+            return 0;
 
-        for (int[] p1 : ones1) {
-            for (int[] p2 : ones2) {
-                int dr = p1[0] - p2[0];
-                int dc = p1[1] - p2[1];
+        int ans = 0;
 
-                int key = dr * 100 + dc;
-
-                int count = frequency.merge(key, 1, Integer::sum);
-                maxOverlap = Math.max(maxOverlap, count);
+        // First 1 of img2 can be outside img1
+        for(int i = -n + 1; i < n; i++)
+        {
+            for(int j = -m + 1; j < m; j++)
+            {
+                ans = Math.max(ans, check(path, img1, i, j));
             }
         }
 
-        return maxOverlap;
+        return ans;
     }
 }
